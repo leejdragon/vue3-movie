38b89ef6f1577c2012c71340d1a9bdcf569b9a87
leejdragon <leejdragon@naver.com>
2021-08-09 16:02:08 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: '이정용',
+    email: 'leejdragon@naver.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
